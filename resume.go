69bba181b409e863eb5d0db23a1713c7d3f994fe
Martin Lindenberg <martinlindenberg@users.noreply.github.com>
2019-04-30 09:35:47 +0200

    Create resume.go

---
+++ resume.go
@@ -0,0 +1,11 @@
+// Martin Lindenberg
+//
+// Senior Software Engineer
+package resume
+
+const (
+	Name     = "Martin Lindenberg"
+	Email    = ""
+	Phone    = ""
+	Location = "Berlin, Fredersdorf"
+)
